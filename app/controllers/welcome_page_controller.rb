73d8f48b452960ec7a2fc85615bccac9f097b86a
Byron Weiss <byron.weiss@gmail.com>
2016-04-06 21:57:27 +0000

    part of rubyengine in welcome contr

--- app/controllers/welcome_page_controller.rb
+++ app/controllers/welcome_page_controller.rb
@@ -1,9 +1,27 @@
 class WelcomePageController < ApplicationController
   require 'alexa_rubyengine'
   
-  def welcome
-  end
+  require 'sinatra'
+  require 'json'
+  require 'bundler/setup'
+  require 'alexa_rubykit' 
   
+  def welcome
+
+
+    # We must return application/json as our content type.
+    before do
+      content_type('application/json')
+    end
+  
+  #enable :sessions
+    post '/' do  
+      request_json = JSON.parse(request.body.read.to_s)
+      raise request_json.inspect
+    end
+  
+  end
+
 end
 
 
